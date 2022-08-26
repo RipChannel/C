51dd1f35ee827803ff69482454d69a29feba1819
Andrey <mr.super.gorn@mail.ru>
2022-08-26 23:21:47 +1000

    Добавил семинар 6

---
+++ ДЗ к семинару 6/Задача 41/Program.cs
@@ -0,0 +1,16 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Введите колличество чисел");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите числа");
+int[] array = new int[size];
+int count = 0;
+for(int i = 0; i < size; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    if(array[i] > 0)
+    {
+      count = count + 1;
+    }
+
+}
+Console.WriteLine($"Чисел больше 0: {count}");
---
+++ ДЗ к семинару 6/Задача 43/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+Console.WriteLine("Введите k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+double x = FindX(k1, b1, k2, b2);
+double y = FindY(k1, b1, x);
+Console.WriteLine($"Точка пересечения двух прямых:= {x}, {y}");
+
+double FindX(int k1, int b1, int k2, int b2)
+{
+int k = k2 * (-1) + k1;
+int b = b2 + b1 * (-1);
+return b/k;
+}
+double FindY(int k1, int b1, double x)
+{
+    return k1 * x + b1;
+}
